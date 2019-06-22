f96da0089a63a8a2ee12350f7f221ee330488ce3
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-06-22 18:50:30 -0300

    Add basic user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import BaseUser
+from .forms import BaseUserCreationForm, BaseUserChangeForm
+
+
+class AdminUser(UserAdmin):
+    add_form = BaseUserCreationForm
+    form = BaseUserChangeForm
+    model = BaseUser
+    list_display = ['email', 'username']
+
+
+admin.site.register(BaseUser, AdminUser)
