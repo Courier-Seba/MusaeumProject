e56d9b980c87e00bd60038bb99b249a243894930
leandrotedini <43319919+leandrotedini@users.noreply.github.com>
2019-07-23 19:03:07 -0300

    Author model (#19)
    * Authors model added
    
    * Delete 0003_auto_20190723_0041.py
    
    Not needed
    
    * Update serializers.py
    
    * Delete serializers.py
    
    * Add serializers of author.
    
    * Create Viewsets and add them to router.
    
    * Permissions added to AuthorViewSet
    
    * Ligth version of serializers added
    
    * Delete 0002_auto_20190723_1652.py
    
    * Delete 0001_initial.py
    
    * Delete 0001_initial.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,19 +4,41 @@ from museum.models import Museum, Artifact, Author
 
 
 class MuseumSerializer(serializers.ModelSerializer):
+    """Museum serializer wich provide all data"""
 
     class Meta:
         model = Museum
         fields = '__all__'
 
 
+class MuseumLigthSerializer(serializers.ModelSerializer):
+    """A ligth version of MuseumSerializer. Provide id and name field only"""
+
+    class Meta:
+        model = Museum
+        fields = ('id', 'short_name')
+        read_only_fields = ('id', 'short_name')
+
+
 class ArtifactSerializer(serializers.ModelSerializer):
+    """Artifact serializer wich provide all data"""
 
     class Meta:
         model = Artifact
         fields = '__all__'
 
+
+class ArtifactLigthSerializer(serializers.ModelSerializer):
+    """A ligth version of ArtifactSerializer. Provide id and name field only"""
+
+    class Meta:
+        model = Artifact
+        fields = ('id', 'name')
+        read_only_fields = ('id', 'name')
+
+
 class AuthorSerializer(serializers.ModelSerializer):
+    """Author serializer wich provide all data"""
 
     class Meta:
         model = Author
