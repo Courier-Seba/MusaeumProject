afeaa82bbcc4777aa8622a529b9a400bb908c82a
leandrotedini <43319919+leandrotedini@users.noreply.github.com>
2019-07-29 18:35:15 -0300

    Rest auth (#23)
    * Add rest auth
    
    * Add the rest_auth urls
    
    * JWT added.

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import obtain_jwt_token
+
 
 from . import views
 
@@ -10,3 +13,8 @@ router.register('author', views.AuthorViewSet, base_name='author')
 
 # Urls
 urlpatterns = router.urls
+urlpatterns += [
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^api-token-auth/', obtain_jwt_token)
+]
