36721484ae5b8e90aa1a6c5211978e9b9a0ec0bc
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-08-22 11:19:38 -0300

    Add mobile menu toggle

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="hero-head">
     <nav class="navbar is-transparent">
-      <div class="navbar-brand"></div>
-      <div class="navbar-start"></div>
+      <div class="navbar-brand">
+        <div class="navbar-item">
+          <a>
+            <img src="/favicon.ico" alt="Logo" />
+          </a>
+        </div>
+        <button
+          class="navbar-burger burger"
+          :class="{ 'is-active': mobileMenuActive }"
+          aria-label="menu"
+          aria-expanded="false"
+          @click="openMobileMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </button>
+      </div>
+      <div class="navbar-menu" :class="{ 'is-active': mobileMenuActive }">
+        <div class="navbar-start"></div>
+        <div class="navbar-end"></div>
+      </div>
     </nav>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TheHeader"
+  name: "TheHeader",
+  data() {
+    return {
+      mobileMenuActive: false
+    };
+  },
+  methods: {
+    openMobileMenu: function() {
+      this.mobileMenuActive = !this.mobileMenuActive;
+    }
+  }
 };
 </script>
