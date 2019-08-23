d8a545f0731bd1055305437f3566789df496f3ba
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-08-23 18:14:53 -0300

    Add raw museum display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,26 @@
 <template>
-  <div class="hero-body"></div>
+  <div class="hero-body">
+    <div class="columns is-multiline">
+      <div v-for="museum in museumList" class="column is-narrow">
+        <app-box :title="museum.short_name" :img="museum.logo"> </app-box>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+import AppBox from "@/components/AppBox";
+export default {
+  name: "Home",
+  components: {
+    AppBox
+  },
+  created() {
+    this.$store.dispatch("getMuseumList");
+  },
+  computed: {
+    ...mapGetters(["museumList"])
+  }
+};
+</script>
