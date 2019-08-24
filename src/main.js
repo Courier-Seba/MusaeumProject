d9527840b261c4528b91473e86c55a9138878ecf
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-08-24 14:21:11 -0300

    Define how to struct icon zone

--- src/main.js
+++ src/main.js
@@ -3,12 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./style.scss";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-library.add(faSearch);
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+import "./assets/icons.js";
 
 Vue.config.productionTip = false;
 
