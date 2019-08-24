1d3ef62f3dda82bffed9c45ec0876d0eee3f2074
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-08-23 21:01:20 -0300

    Add  icons

--- src/components/AppSearchBar.vue
+++ src/components/AppSearchBar.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="field">
-    <div class="control">
-      <input class="input is-rounded" type="text">
+    <div class="control has-icons-left">
+      <input class="input is-rounded" type="text" />
+      <span class="icon is-small is-left">
+        <font-awesome-icon icon="search"></font-awesome-icon>
+      </span>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from "./router";
 import store from "./store";
 import "./style.scss";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faSearch);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
