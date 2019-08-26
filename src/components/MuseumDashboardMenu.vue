6f38666f89cd15223235c1717ecd7820a7af0300
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-08-26 11:22:50 -0300

    Fix admin menu

--- src/components/MuseumDashboardMenu.vue
+++ src/components/MuseumDashboardMenu.vue
@@ -1,6 +1,24 @@
 <template>
   <aside class="menu" id="menu">
-    <p class="menu-label">My museum</p>
+    <p class="menu-label">Mi museo</p>
+    <ul class="menu-list">
+      <li><a>Muestra</a></li>
+      <li><a>Configuración</a></li>
+    </ul>
+    <p class="menu-label">
+      Administración del museo
+    </p>
+    <ul class="menu-list">
+      <li><a>Información del museo</a></li>
+      <li>
+        <a>Tus colecciónes</a>
+      </li>
+      <li><a>Estrellas y comentarios</a></li>
+      <p class="menu-label">
+        Mi cuenta
+      </p>
+      <li><a>Información de la cuenta</a></li>
+    </ul>
   </aside>
 </template>
 
