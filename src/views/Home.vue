bd097c2e75ade491729eaa9192de150ff1012782
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-08-27 22:09:24 -0300

    Add carousel to column

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,17 @@
 <template>
-<div>
   <div>
-      <app-carousel> </app-carousel>
-    </div>
-  <div class="columns is-multiline is-main-content is-fullheight">
-    <div class="column is-full">
-      <div class="columns is-multiline">
-        <div v-for="museum in museumList" class="column is-narrow">
-          <app-box :title="museum.short_name" :img="museum.logo"> </app-box>
+    <div class="columns is-multiline is-main-content is-fullheight">
+      <div class="column is-full">
+          <app-carousel> </app-carousel>
+      </div>
+      <div class="column is-full">
+        <div class="columns is-multiline">
+          <div v-for="artifact in artifactList" class="column is-narrow">
+            <app-box :title="artifact.name" :img="artifact.picture"> </app-box>
+          </div>
         </div>
       </div>
     </div>
-    <div class="column is-full">
-      <div class="columns is-multiline">
-        <div v-for="artifact in artifactList" class="column is-narrow">
-          <app-box :title="artifact.name" :img="artifact.picture"> </app-box>
-        </div>
-      </div>
-    </div>
-   </div>
   </div>
 </template>
 
