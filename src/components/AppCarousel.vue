a0ffb2542ec60d28d6c448e545bdd4cd0bdb81b7
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-08-27 22:41:41 -0300

    Add image to slide

--- src/components/AppCarousel.vue
+++ src/components/AppCarousel.vue
@@ -1,27 +1,36 @@
 <template>
   <vueper-slides>
-    <vueper-slide v-for="(slide, i) in slides" :key="i" :title="slide.title" :content="slide.content"></vueper-slide>
+    <vueper-slide
+      v-for="(museum, i) in museumList"
+      :key="i"
+      :title="museum.short_name"
+      :image="museum.front_picture"
+    ></vueper-slide>
   </vueper-slides>
 </template>
 
 <script>
-import { VueperSlides, VueperSlide } from 'vueperslides';
-import 'vueperslides/dist/vueperslides.css';
+import { mapGetters } from "vuex";
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
 
 export default {
-  name:"AppCarousel",
+  name: "AppCarousel",
   components: { VueperSlides, VueperSlide },
   data: () => ({
-  slides: [
-    {
-      title: 'Slide #1',
-      content: 'Slide content.'
-    },
-    {
-      title: 'Slide #2',
-      content: 'Slide content.'
-    }
-  ]
-})
-}
+    slides: [
+      {
+        title: "Slide #1",
+        content: "Slide content."
+      },
+      {
+        title: "Slide #2",
+        content: "Slide content."
+      }
+    ]
+  }),
+  computed: {
+    ...mapGetters(["museumList"])
+  }
+};
 </script>
