bd280c5771e0a11977c2faa9578253df8b4e51f2
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-08-30 18:06:08 -0300

    Fix registration guide button

---
+++ src/components/HeaderSignUpGuideStep2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="section">
+    <b-button @click="done"></b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderSignUpGuideStep2",
+  methods: {
+    done: function() {
+      this.$emit("ready");
+    }
+  }
+};
+</script>
