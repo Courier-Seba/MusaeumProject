cea2b650fa28c08e9d08a62192883567d3ca28f3
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-01 19:25:26 -0300

    Add children router of admin dashboard

--- src/components/MuseumDashboardMenu.vue
+++ src/components/MuseumDashboardMenu.vue
@@ -1,27 +1,33 @@
 <template>
   <b-menu id="menu">
-        <b-menu-list label="Menu">
-            <b-menu-item icon="information-outline" label="Home"></b-menu-item>
-            <b-menu-item icon="settings" active expanded>
-                <template slot="label" slot-scope="props">
-                    Configuración
-                    <b-icon
-                        class="is-pulled-right"
-                        :icon="props.expanded ? 'menu-down' : 'menu-up'">
-                    </b-icon>
-                </template>
-                <b-menu-item label="Mi perfil"></b-menu-item>
-                <b-menu-item label="Mis favoritos"></b-menu-item>
-            </b-menu-item>
-            <b-menu-item icon="account" label="Mis Museos">
-                <b-menu-item label="Mi museo"></b-menu-item>
-                <b-menu-item label="Datos del usuario"></b-menu-item>
-            </b-menu-item>
-        </b-menu-list>
-        <b-menu-list label="Acciones">
-            <b-menu-item label="Salir"></b-menu-item>
-        </b-menu-list>
-    </b-menu>
+    <b-menu-list label="Menu">
+      <router-link to="/home">
+        <b-menu-item icon="information-outline" label="Home"></b-menu-item>
+      </router-link>
+      <b-menu-item icon="settings" active expanded>
+        <template slot="label" slot-scope="props">
+          Configuración
+          <b-icon
+            class="is-pulled-right"
+            :icon="props.expanded ? 'menu-down' : 'menu-up'"
+          >
+          </b-icon>
+        </template>
+
+        <router-link to="/profile">
+          <b-menu-item label="Mi perfil"></b-menu-item>
+        </router-link>
+        <b-menu-item label="Mis favoritos"></b-menu-item>
+      </b-menu-item>
+      <b-menu-item icon="account" label="Mis Museos">
+        <b-menu-item label="Mi museo"></b-menu-item>
+        <b-menu-item label="Datos del usuario"></b-menu-item>
+      </b-menu-item>
+    </b-menu-list>
+    <b-menu-list label="Acciones">
+      <b-menu-item label="Salir"></b-menu-item>
+    </b-menu-list>
+  </b-menu>
 </template>
 
 <style>
