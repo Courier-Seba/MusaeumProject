37b7fa4da98f929960503c2170a034a75994b011
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-09-03 11:38:27 -0300

    Add museum list view

---
+++ src/components/AppMuseumLevel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="level">
+    <div class="level-left"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppMuseumLevel",
+  props: {
+    shortName: String,
+    country: String,
+    city: String,
+    logo: String,
+    museumLevel: String,
+    museumType: String,
+    user: Number
+  }
+};
+</script>
---
+++ src/views/MuseumListView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <app-museum-level v-for="museum in museumList" :key="museum.pk"></app-museum-level>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import AppMuseumLevel from "@/components/AppMuseumLevel";
+export default {
+  name: "MuseumListView",
+  components: {
+    AppMuseumLevel
+  },
+  computed: {
+    ...mapGetters(["museumList"])
+  }
+};
+</script>
