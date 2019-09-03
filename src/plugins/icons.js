e0abbaebb52a817a8c997be6b6eba1a84773496d
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-03 16:49:02 -0300

    Use workaourd to fix buefy

--- src/plugins/icons.js
+++ src/plugins/icons.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch, faUser, fas } from "@fortawesome/free-solid-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-library.add(faSearch);
-library.add(faUser);
 library.add(fas);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
