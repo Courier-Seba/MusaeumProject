0c1e2d3b6482868ca08df3f617cead2ff9be1d3e
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-09-06 09:05:18 -0300

    Refactor i18 strings for dashboard

--- src/components/MuseumDashboardMenu.vue
+++ src/components/MuseumDashboardMenu.vue
@@ -1,28 +1,33 @@
 <template>
   <b-menu id="menu">
-        <b-menu-list label="Menu">
-            <b-menu-item icon="information-outline" :label="$t('dashboardItem01')"></b-menu-item>
-            <b-menu-item icon="settings" active expanded>
-                <template slot="label" slot-scope="props">
-                   {{ $t("dashboardItem02")}}
-                    <b-icon
-                        class="is-pulled-right"
-                        :icon="props.expanded ? 'menu-down' : 'menu-up'">
-                    </b-icon>
-                </template>
-                <b-menu-item :label="$t('dashboardItem03')"></b-menu-item>
-                <b-menu-item :label="$t('dashboardItem04')"></b-menu-item>
-                <b-menu-item :label="$t('dashboardItem05')"></b-menu-item>
-            </b-menu-item>
-            <b-menu-item icon="account" :label="$t('dashboardItem06')">
-                <b-menu-item :label="$t('dashboardItem07')"></b-menu-item>
-                <b-menu-item :label="$t('dashboardItem08')"></b-menu-item>
-            </b-menu-item>
-        </b-menu-list>
-        <b-menu-list :label="$t('dashboardItem09')">
-            <b-menu-item :label="$t('dashboardItem10')"></b-menu-item>
-        </b-menu-list>
-    </b-menu>
+    <b-menu-list label="Menu">
+      <b-menu-item
+        icon="information-outline"
+        :label="$t('dashboard.menu.home')"
+      ></b-menu-item>
+      <b-menu-item icon="settings" active expanded>
+        <template slot="label" slot-scope="props">
+          {{ $t("dashboard.menu.profile") }}
+          <b-icon
+            class="is-pulled-right"
+            :icon="props.expanded ? 'menu-down' : 'menu-up'"
+          >
+          </b-icon>
+        </template>
+        <b-menu-item :label="$t('dashboard.menu.config')"></b-menu-item>
+        <b-menu-item :label="$t('dashboard.menu.profileData')"></b-menu-item>
+        <b-menu-item :label="$t('dashboard.menu.favourites')"></b-menu-item>
+      </b-menu-item>
+      <b-menu-item icon="account" :label="$t('dashboard.menu.myMuseum')">
+        <b-menu-item :label="$t('dashboard.menu.info')"></b-menu-item>
+        <b-menu-item :label="$t('dashboard.menu.myCollections')"></b-menu-item>
+        <b-menu-item :label="$t('dashboard.menu.actions')"></b-menu-item>
+      </b-menu-item>
+    </b-menu-list>
+    <b-menu-list :label="$t('dashboard.menu.exit')">
+      <b-menu-item :label="$t('dashboard.menu.logOut')"></b-menu-item>
+    </b-menu-list>
+  </b-menu>
 </template>
 
 <style>
