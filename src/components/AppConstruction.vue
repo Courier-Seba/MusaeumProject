69be5375bf5381f339a0d0dec70c29697345b1d7
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-09-06 10:11:57 -0300

    Add under contruction component and add a route

---
+++ src/components/AppConstruction.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container has-text-centered" id="construction">
+    <figure class="image container is-128x128">
+      <img src="/assets/under_construction.png" />
+    </figure>
+    <h1 class="has-text-black">
+      This place still in developement please hold tight
+    </h1>
+  </div>
+</template>
+
+<style>
+#construction {
+  margin-top: 10rem
+}
+</style>
