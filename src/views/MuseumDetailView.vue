b5d86714132f8e1e1fcacb5ef7f1adf460e442d7
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-06 18:36:44 -0300

    Add render museum title

--- src/views/MuseumDetailView.vue
+++ src/views/MuseumDetailView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="columns">
+  <div class="columns is-vcentered">
+    <div class="column is-full has-text-centered">
+      <h1 class="is-size-2">
+      {{ $t("museumView.welcome") }} {{ museumDetail.short_name }}
+      </h1>
+    </div>
   </div>
 </template>
 
