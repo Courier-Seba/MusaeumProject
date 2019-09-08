1669e7bf31f2b2e720355da3a5d3eeae2f8672ce
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-08 18:36:26 -0300

    Refactor code to desentralize

---
+++ artifact/serializers.py
@@ -0,0 +1,30 @@
+"""
+# Api serializers of artifact
+This module serializate the models to pass as json in the api.
+"""
+from rest_framework import serializers
+from .models import Artifact, ArtifactTag
+
+class ArtifactSerializer(serializers.ModelSerializer):
+    """Artifact serializer wich provide all data"""
+
+    class Meta:
+        model = Artifact
+        fields = '__all__'
+
+
+class ArtifactTagSerializer(serializers.ModelSerializer):
+    """Artifact tag serializers with only the name"""
+
+    class Meta:
+        model = ArtifactTag
+        fields = '__all__'
+
+
+class ArtifactLigthSerializer(serializers.ModelSerializer):
+    """A ligth version of ArtifactSerializer. Provide id and name field only"""
+
+    class Meta:
+        model = Artifact
+        fields = ('id', 'name')
+        read_only_fields = ('id', 'name')
--- artifact/views.py
+++ artifact/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+"""
+# Views
+This module contains the views of the app.
+"""
 
 # Create your views here.
+from rest_framework import (viewsets, filters, permissions)
+from rest_framework import generics
+
+# Serializers
+from api.permissions import IsOwnerOrReadOnly, IsAdministratorOrReadOnly
+from artifact.models import Artifact, ArtifactTag
+
+from .serializers import ArtifactSerializer, ArtifactTagSerializer
+
+class ArtifactViewSet(viewsets.ModelViewSet):
+    """
+    ## Api view set of the artifact model
+    Search for name
+    """
+    search_fields = ['name', ]
+    filter_backends = (filters.SearchFilter,)
+    queryset = Artifact.objects.all()
+    serializer_class = ArtifactSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
+class ArtifactTagViewSet(viewsets.ModelViewSet):
+    """
+    ## Api view set of the tags for artifacts
+    """
+    queryset = ArtifactTag.objects.all()
+    serializer_class = ArtifactTagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
+
+
