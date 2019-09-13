6b95a9164e472b4a53bb62d2c0569b50d4a6e26e
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-13 17:05:30 -0300

    Add docs to app collection

--- src/views/MuseumDetailView.vue
+++ src/views/MuseumDetailView.vue
@@ -1,3 +1,5 @@
+This is a museum detailed view with their collections, artifact, address (if
+any), info about and the username
 <template>
   <div class="columns is-vcentered">
     <div class="column is-full has-text-centered">
@@ -5,7 +7,12 @@
         {{ $t("museumView.welcome") }} {{ museumDetail.short_name }}
       </h1>
       <h1 class="is-size-4">{{ $t("museumView.artifactIntro") }}</h1>
-      <div class="section" v-for="(collection, i) in museumCollections" :key="i">
+      <div
+        class="section"
+        v-for="(collection, i) in museumCollections"
+        :key="i"
+      >
+        <h1 class="is-size-5 has-text-cente">{{ collection.title }}</h1>
         <app-collection-carousel
           :collectionId="collection.id"
         ></app-collection-carousel>
