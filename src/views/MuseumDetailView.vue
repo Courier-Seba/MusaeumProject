8173064c70fad6f8c15a4004efaa18203ee04e2c
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-13 16:58:01 -0300

    Add collection slider

--- src/views/MuseumDetailView.vue
+++ src/views/MuseumDetailView.vue
@@ -2,16 +2,26 @@
   <div class="columns is-vcentered">
     <div class="column is-full has-text-centered">
       <h1 class="is-size-2">
-      {{ $t("museumView.welcome") }} {{ museumDetail.short_name }}
+        {{ $t("museumView.welcome") }} {{ museumDetail.short_name }}
       </h1>
+      <h1 class="is-size-4">{{ $t("museumView.artifactIntro") }}</h1>
+      <div class="section" v-for="(collection, i) in museumCollections" :key="i">
+        <app-collection-carousel
+          :collectionId="collection.id"
+        ></app-collection-carousel>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import AppCollectionCarousel from "@/components/AppCollectionCarousel";
 export default {
   name: "MuseumDetailView",
+  components: {
+    AppCollectionCarousel
+  },
   computed: {
     ...mapGetters(["museumDetail", "museumCollections"])
   },
@@ -23,7 +33,7 @@ export default {
   },
   mounted() {
     this.getMuseumData(this.id);
-    this.getMuseumCollections(this.id)
+    this.getMuseumCollections(this.id);
   }
 };
 </script>
