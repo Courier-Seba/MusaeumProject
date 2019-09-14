d9789667afc97c186d91b248b9998cf28ff770d2
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-09-13 22:23:39 -0300

    Add icons to museum

--- src/components/MuseumDashboardMenu.vue
+++ src/components/MuseumDashboardMenu.vue
@@ -2,17 +2,17 @@
   <b-menu id="menu">
     <b-menu-list label="Menu">
       <b-menu-item
-        icon="information-outline"
+        icon="home"
         :label="$t('dashboard.menu.home')"
         tag="router-link"
         to="/my-museum/construction"
       ></b-menu-item>
-      <b-menu-item icon="settings" active expanded>
+      <b-menu-item  active expanded >
         <template slot="label" slot-scope="props">
           {{ $t("dashboard.menu.profile") }}
           <b-icon
             class="is-pulled-right"
-            :icon="props.expanded ? 'menu-down' : 'menu-up'"
+            :icon="props.expanded ? 'arrow-circle-up' : 'arrow-circle-down'"
           >
           </b-icon>
         </template>
@@ -20,18 +20,27 @@
           :label="$t('dashboard.menu.config')"
           to="/my-museum/configuration"
           tag="router-link"
+          icon="cogs"
         ></b-menu-item>
-        <b-menu-item :label="$t('dashboard.menu.profileData')"></b-menu-item>
-        <b-menu-item :label="$t('dashboard.menu.favourites')"></b-menu-item>
+        <b-menu-item :label="$t('dashboard.menu.profileData')" icon="id-badge"></b-menu-item>
+        <b-menu-item :label="$t('dashboard.menu.favourites')" icon="star"></b-menu-item>
       </b-menu-item>
-      <b-menu-item icon="account" :label="$t('dashboard.menu.myMuseum')">
-        <b-menu-item :label="$t('dashboard.menu.info')"></b-menu-item>
+      <b-menu-item>
+        <template slot="label" slot-scope="props">
+          {{ $t("dashboard.menu.myMuseum") }}
+          <b-icon
+            class="is-pulled-right"
+            :icon="props.expanded ? 'arrow-circle-up' : 'arrow-circle-down'"
+          >
+          </b-icon>
+        </template>
+        <b-menu-item icon="drafting-compass" :label="$t('dashboard.menu.info')"></b-menu-item>
         <b-menu-item :label="$t('dashboard.menu.myCollections')"></b-menu-item>
         <b-menu-item :label="$t('dashboard.menu.actions')"></b-menu-item>
       </b-menu-item>
     </b-menu-list>
     <b-menu-list :label="$t('dashboard.menu.exit')">
-      <b-menu-item :label="$t('dashboard.menu.logOut')"></b-menu-item>
+      <b-menu-item :label="$t('dashboard.menu.logOut')" icon="user-times"></b-menu-item>
     </b-menu-list>
     <div id="bottom">
       <b-button
