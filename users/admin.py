7142a60a592cbe756c8ea8db9a21baaab9f2e9f3
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-15 16:51:42 -0300

    Add new data to user

--- users/admin.py
+++ users/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .models import BaseUser
+from .models import User
 from .forms import BaseUserCreationForm, BaseUserChangeForm
 
 
 class AdminUser(UserAdmin):
     add_form = BaseUserCreationForm
     form = BaseUserChangeForm
-    model = BaseUser
+    model = User
     list_display = ['email', 'username']
 
 
-admin.site.register(BaseUser, AdminUser)
+admin.site.register(User, AdminUser)
