dfe3d2ce6e54f5b69e43c8a08fcda220fa75fde3
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-16 15:02:23 -0300

    Add internationalization and fix image

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="columns is-multiline">
-    <div class="column is-full">
-      <h1 class="is-size-3">{{ $t("aboutUs.title") }}</h1>
+  <div class="columns is-multiline" id="about-us-container">
+    <div class="column is-full has-text-centered" id="about-us-title">
+      <h1 class="title is-size-1">{{ $t("aboutUsView.title") }}</h1>
     </div>
-    <div class="column is-three-quarters">
-      <figure class="image is-5by3">
-        <img src="/assets/los-giles" alt="Ups" />
+    <div class="column is-two-quarters" id="about-us-pic">
+      <figure class="image is-3by2">
+        <img src="/assets/los-giles.png" alt="Ups" />
       </figure>
     </div>
-    <div class="column is-one-quarters">
-      <p>Info</p>
+    <div class="column is-one-quarters" id="about-us-intro">
+      <p>{{ $t("aboutUsView.intro") }}</p>
     </div>
+    <div class="column is-full">
+      <h1 class="subtitle">{{ $t("aboutUsView.objetiveTitle") }}</h1>
+      <p class="has-text-black">{{ $t("aboutUsView.objetive") }}</p>
+    </div>
+    <div class="column is-half">
+      <h1 class="subtitle">{{ $t("aboutUsView.processTitle") }}</h1>
+      <p class="has-text-black">{{ $t("aboutUsView.process") }}</p>
+    </div>
+    <div class="column is-half"></div>
   </div>
 </template>
+
+<style>
+#about-us-container {
+  padding: 2.5rem;
+}
+</style>
