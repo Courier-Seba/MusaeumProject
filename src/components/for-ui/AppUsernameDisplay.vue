153c231321013be349b2270b55945453513e7fb3
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-15 23:46:44 -0300

    Add new buttom for user logged

---
+++ src/components/for-ui/AppUsernameDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-button
+    type="is-primary"
+    tag="router-link"
+    to="/my-museum"
+    icon-left="landmark"
+  >
+    {{ $t("common.welcome") }} {{ userName }}
+  </b-button>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "AppUsernameDisplay",
+  computed: {
+    ...mapGetters(["userName"])
+  }
+};
+</script>
