a39a6e921d40fda6bd89758ff0353cfd7d481cd5
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-09-16 21:21:17 -0300

    Add some bottom lines

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
 Initial and presentational page of the app.
 <template>
-  <div class="columns is-multiline is-main-content ">
-    <div class="column is-full has-text-centered ">
+  <div class="columns is-multiline is-main-content" id="home">
+    <div class="column is-full has-text-centered bottom-line">
       <app-welcome-msg></app-welcome-msg>
     </div>
 
-    <div class="column is-three-quarters">
-      <h1 class="subtitle is-size-3">{{ $t("homeView.museum") }}</h1>
+    <div class="column is-three-quarters bottom-line">
+      <h1 class="subtitle is-size-3 intro-link">{{ $t("homeView.museum") }}</h1>
     </div>
-    <div class="column is-one-quarter">
+    <div class="column is-one-quarter center-content">
       <b-button
         tag="router-link"
         to="/museums"
@@ -19,13 +19,15 @@ Initial and presentational page of the app.
         {{ $t("homeView.museumLink") }}
       </b-button>
     </div>
-    <div class="column is-full" id="museum-carousel">
+    <div class="column is-full bottom-line" id="museum-carousel ">
       <museum-carousel> </museum-carousel>
     </div>
     <div class="column is-three-quarters">
-      <h1 class="subtitle is-size-3">{{ $t("homeView.artifact") }}</h1>
+      <h1 class="subtitle is-size-3 intro-link">
+        {{ $t("homeView.artifact") }}
+      </h1>
     </div>
-    <div class="column is-one-quarter">
+    <div class="column is-one-quarter center-content">
       <b-button
         tag="router-link"
         to="/artifacts"
@@ -36,8 +38,8 @@ Initial and presentational page of the app.
         {{ $t("homeView.artifactLink") }}
       </b-button>
     </div>
-    <div class="column is-full">
-      <div class="columns is-multiline">
+    <div class="column is-full bottom-line">
+      <div class="columns is-multiline" id="artifact-list">
         <div v-for="(artifact, i) in artifactList" class="column is-2" :key="i">
           <app-artifact-box
             :artifact="artifact"
@@ -67,3 +69,21 @@ export default {
   }
 };
 </script>
+
+<style>
+#artifact-list {
+  padding-right: 3rem;
+  padding-left: 3rem;
+}
+.intro-link {
+  padding-left: 2rem;
+}
+.center-content {
+  text-align: center;
+}
+.bottom-line {
+  border-bottom: 1px solid #1f1209;
+  box-shadow: 0 20px 20px -20px #333;
+  margin-bottom: 1.7rem;
+}
+</style>
