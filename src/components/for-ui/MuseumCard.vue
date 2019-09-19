3d978e580d478309fa436a0ef6ab34506387f12f
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-09-19 19:29:50 -0300

    Add columns to museum cards

--- src/components/for-ui/MuseumCard.vue
+++ src/components/for-ui/MuseumCard.vue
@@ -3,41 +3,47 @@ Card for a presentation of a museum.
   <div class="museum-level">
     <div class="columns is-centered">
       <div class="column is-two-thirds">
-        <div class="media">
-          <div class="media-left">
-            <figure class="image is-64x64">
-              <img :src="logo" alt="" />
-            </figure>
-          </div>
-          <div class="media-content">
-            <div class="has-text-justified has-text-black">
-              <h1 class="is-capitalized has-text-weight-semibold is-size-2">
-                {{ shortName }}
-              </h1>
+        <div class="columns is-multiline museum-info">
+          <div class="column is-full">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-64x64">
+                  <img :src="logo" alt="" />
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="has-text-justified has-text-black">
+                  <h1 class="is-capitalized has-text-weight-semibold is-size-2">
+                    {{ shortName }}
+                  </h1>
+                </div>
+                <h3 class="is-size-3">
+                  {{ level }}
+                </h3>
+                <p class="is-capitalized has-text-black is-size-5">
+                  {{ $t("museumBox.museumZone.country") }} {{ country }}
+                </p>
+                <p class="is-capitalized has-text-black is-size-5">
+                  {{ $t("museumBox.museumZone.city") }} {{ city }}
+                </p>
+              </div>
+              <div class="media-right" @click="giveStar">
+                <b-icon icon="star" :type="starType"></b-icon>
+              </div>
             </div>
-            <h3 class="is-size-3">
-              {{ level }}
-            </h3>
-            <p class="is-capitalized has-text-black is-size-5">
-              {{ $t("museumBox.museumZone.country") }} {{ country }}
-            </p>
-            <p class="is-capitalized has-text-black is-size-5">
-              {{ $t("museumBox.museumZone.city") }} {{ city }}
-            </p>
-          </div>
-          <div class="media-right" @click="giveStar">
-            <b-icon icon="star" :type="starType"></b-icon>
           </div>
         </div>
-        <div class="visit-button has-text-centered">
-          <b-button
-            tag="router-link"
-            :to="link"
-            type="is-link"
-            size="is-large"
-            icon-right="angle-double-right"
-            >{{ $t("museumBox.visit") }}</b-button
-          >
+        <div class="column is-full">
+          <div class="visit-button has-text-centered">
+            <b-button
+              tag="router-link"
+              :to="link"
+              type="is-link"
+              size="is-large"
+              icon-right="angle-double-right"
+              >{{ $t("museumBox.visit") }}</b-button
+            >
+          </div>
         </div>
       </div>
       <div class="column is-one-thirds">
@@ -105,7 +111,7 @@ export default {
   box-shadow: 0px 10px 19px 5px rgba(0,0,0,0.75);
   margin: 2.50rem
 }
-.visit-button {
-  margin-top: 6.5rem;
+.museum-info {
+  margin: 1rem;
 }
 </style>
