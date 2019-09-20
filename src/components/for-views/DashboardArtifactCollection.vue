019921970f3676de33baa702fdaf7062221d7ec8
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-09-20 11:00:22 -0300

    Add collection upload module

---
+++ src/components/for-views/DashboardArtifactCollection.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-field>
+    <b-input
+      placeholder="$t('dashboard.artifactView.collectionPlaceholder')"
+      v-model="title"
+    >
+    </b-input>
+    <span class="control">
+      <b-button
+        @click="createCollection"
+        icon-right="plus"
+        type="is-primary"
+      ></b-button>
+    </span>
+  </b-field>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "DashboardArtifactCollection",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    ...mapActions(["postUserCollection"]),
+    createCollection: function() {
+      postUserCollection(this.title)
+    }
+  }
+};
+</script>
