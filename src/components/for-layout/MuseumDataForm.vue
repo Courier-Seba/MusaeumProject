6ee452178fb33ba4ac06664afc4782b30cc37c23
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-09-20 21:52:59 -0300

    Add more fields to museum form

--- src/components/for-layout/MuseumDataForm.vue
+++ src/components/for-layout/MuseumDataForm.vue
@@ -1,17 +1,29 @@
 This component modify user museum data in store.
 <template>
-  <div>
-    <b-field :label="$t('museumForm.shortName')" label-position="on-border">
-      <b-input v-model="shortName"></b-input>
-      <b-button>{{ $t("museumForm.update") }}</b-button>
-    </b-field>
-    <b-field :label="$t('museumForm.shortName')" label-position="on-border">
-      <b-input v-model="shortName"></b-input>
-    </b-field>
-    <b-field :label="$t('museumForm.logo')">
-      <b-upload>
-      </b-upload>
-    </b-field>
+  <div class="columns">
+    <div class="column is-one-quarter">
+      <b-field :label="$t('museumForm.logo')">
+        <b-upload v-model="logo">
+          <b-button icon-right="exchange-alt"></b-button>
+        </b-upload>
+      </b-field>
+    </div>
+    <div class="column is-half">
+      <b-field :label="$t('museumForm.shortName')" label-position="on-border">
+        <b-input v-model="shortName"></b-input>
+        <b-button>{{ $t("museumForm.update") }}</b-button>
+      </b-field>
+      <b-field :label="$t('museumForm.shortName')" label-position="on-border">
+        <b-input v-model="shortName"></b-input>
+      </b-field>
+    </div>
+    <div class="column is-one-quarter">
+      <b-field :label="$t('museumForm.front')">
+        <b-upload v-model="front">
+          <b-button icon-right="exchange-alt"></b-button>
+        </b-upload>
+      </b-field>
+    </div>
   </div>
 </template>
 
@@ -20,7 +32,12 @@ export default {
   name: "MuseumDataForm",
   data() {
     return {
-      shortName: ""
+      shortName: "",
+      longName: "",
+      logo: null,
+      logoUrl: null,
+      front: null,
+      frontUrl: null,
     };
   }
 };
