8c2fbd92a81528ee805a6f29e97a8f890dab484c
Jvtomas98 <jvtomas98@gmail.com>
2019-09-22 14:12:44 -0300

    New spanish presentation

--- src/plugins/translations/es.js
+++ src/plugins/translations/es.js
@@ -6,6 +6,18 @@ export default {
     outro:
       "El conocimiento es un bien público y la historia es patrimonio de la humanidad."
   },
+  aboutUsView: {
+    title: "¿Quienes somos?",
+    intro:
+      "Somos un pequeño grupo de desarrolladores y estudiantes que decidieron crear un lugar para el mundo y con la ayuda la gente compartir sus historias o tradiciones",
+    objetiveTitle: "Nuestr objetivo",
+    objetive:
+      "Nuestro objetivo es simple, queremos compartir y aprender de otras culturas de otros países, sus tradiciones y las historias que tienen. Vos podes ser parte de esto compartiendo tu historia y cultura, y con tu ayuda prodemos hacer que la historia sea solo una.",
+    processTitle: "El proceso",
+    process:
+      "Construimos esto mientras aprendiamos, no fue sencillo. Realizamos errores, aprendimos de ellos y los arreglamos. Fue una tarea de buena intención, no estamos buscando dinero, lo único que nos mantenía fue la motivación.",
+  },
+
 
   navbarItem01: "Museos",
   navbarItem02: "Artefactos",
