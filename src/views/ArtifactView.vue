a74ad709aebf20d954abdafdbf09015c5a067e18
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-09-22 22:19:18 -0300

    Add artifact search by name

--- src/views/ArtifactView.vue
+++ src/views/ArtifactView.vue
@@ -1,27 +1,36 @@
 List of popular artifacts. With a search.
 <template>
-  <div id="artifact-list">
-    <div class="columns is-multiline">
-      <div v-for="(artifact, i) in artifactList" class="column is-2" :key="i">
-        <app-artifact-box
-          :artifact="artifact"
-          :showTitle="true"
-        ></app-artifact-box>
-      </div>
+  <div id="artifact-list" class="columns is-multiline">
+    <div class="column is-full">
+      <artifact-filter-bar></artifact-filter-bar>
+    </div>
+    <div v-for="(artifact, i) in artifactList" class="column is-2" :key="i">
+      <app-artifact-box
+        :artifact="artifact"
+        :showTitle="true"
+      ></app-artifact-box>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 import AppArtifactBox from "@/components/for-ui/AppArtifactBox";
+import ArtifactFilterBar from "@/components/for-views/ArtifactFilterBar";
 export default {
   name: "ArtifactView",
   components: {
-    AppArtifactBox
+    AppArtifactBox,
+    ArtifactFilterBar
   },
   computed: {
     ...mapGetters(["artifactList"])
+  },
+  methods: {
+    ...mapActions(["getArtifactList"])
+  },
+  created() {
+    this.getArtifactList();
   }
 };
 </script>
