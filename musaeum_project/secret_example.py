eeb3f710bbcd28071a233e40204cd23c55ef6ae3
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-10-01 11:47:30 -0300

    Create an example of secret.py

---
+++ musaeum_project/secret_example.py
@@ -0,0 +1,17 @@
+# Secret key
+KEY = '#####long_random_key$'
+
+# Database name
+DATABASE_NAME = 'db_name'
+
+# Database user
+DATABASE_USER = 'db_user'
+
+# Database user password
+DATABASE_PASSWORD = 'db_user_password'
+
+# Database host
+DATABASE_HOST = 'localhost'
+
+# Database host port
+DATABASE_PORT = ''
