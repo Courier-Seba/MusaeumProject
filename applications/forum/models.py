a3950d53bee9385a2fdb159f53e1e6020c8777e0
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-10-18 09:51:16 -0300

    Add md and time fields to forum models

--- applications/forum/models.py
+++ applications/forum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 from musaeum_project.settings import AUTH_USER_MODEL as user
 
 from musaeum_project.database_const import (
@@ -14,6 +15,8 @@ class PostTag(models.Model):
     * name: name of the tag
     """
     name = models.TextField(max_length=TEXT_FIELD_LENGTH)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Post(models.Model):
     """
@@ -24,5 +27,25 @@ class Post(models.Model):
     """
     title = models.CharField(max_length=SHORT_CHARFIEL_LENGTH, null=False)
     tags = models.ManyToManyField(PostTag, blank=True)
+    content = models.FileField(
+        null=False, blank=False,
+        upload_to="forum/posts/",
+        validators=[FileExtensionValidator(["md"])]
+    )
     owner = models.ForeignKey(user, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+class Comment(models.Model):
+    """
+    ## Post/comment Comment
+    * content: text content
+    """
+    content = models.FileField(
+        null=False, blank=True,
+        upload_to="forum/posts/",
+        validators=[FileExtensionValidator(["md"])],
+    )
+    owner = models.ForeignKey(user, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
