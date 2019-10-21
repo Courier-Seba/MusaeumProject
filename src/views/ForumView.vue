cf078abe5bf9a699d6166a73e8f83b2e68300236
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-10-21 18:36:02 -0300

    Create forum post overview

--- src/views/ForumView.vue
+++ src/views/ForumView.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="container">
+    <h1>asdf</h1>
     <div v-for="post in postList" :key="post.id">
+      <forum-post-overview
+        :id="post.id"
+        :title="post.title"
+        :body="post.content"
+      >
+      </forum-post-overview>
     </div>
   </div>
 </template>
 
 <script>
+import ForumPostOverview from "@/components/for-ui/ForumPostOverview";
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "ForumView",
+  components: {
+    ForumPostOverview
+  },
+  methods: {
+    ...mapActions(["getLastestPosts"])
+  },
+  computed: {
+    ...mapGetters(["postList"])
+  },
+  created() {
+    this.getLastestPosts();
+  }
 };
 </script>
