ed264c0b19384bf5c072bcef73fbd5309121d3bb
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-10-21 08:39:18 -0300

    Add orderig post by lastest

--- applications/forum/views.py
+++ applications/forum/views.py
@@ -14,32 +14,38 @@ from .serializers import PostSerializer, PostTagSerializer, CommentSerializer
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
     """
-    ## Api view set of the posts
+    ## Api view set of the posts ordered by lastest
     Search by title
     """
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ['title',]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    )
+    ordering_fields = ('created_at')
+    ordering = ('-created_at')
+    search_fields = ['title',]
 
 class PostTagViewSet(viewsets.ModelViewSet):
     """
-    ## Api view set of the posts
+    ## Api view set of the posts tags
     Search by title
     """
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ['name',]
     queryset = PostTag.objects.all()
     serializer_class = PostTagSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['name',]
 
 class CommentViewSet(viewsets.ModelViewSet):
     """
     ## Api view set of the comment for a post
     """
-    filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ['post']
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ['post']
