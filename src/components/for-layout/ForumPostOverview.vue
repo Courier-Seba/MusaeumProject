ddb08abdd853e2fe18c4a238b88dc387e2ed8078
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-10-22 09:40:54 -0300

    Add toggle feature to forum post

--- src/components/for-layout/ForumPostOverview.vue
+++ src/components/for-layout/ForumPostOverview.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="post">
+  <div
+    class="post"
+    :class="isOpen ? 'displayed' : 'hidden'"
+    @click="displayPost"
+  >
     <div class="media">
       <div class="media-content">
         <h1 class="subtitle is-size-2">{{ title }}</h1>
@@ -23,7 +27,8 @@ export default {
   name: "ForumPostOverview",
   data() {
     return {
-      mdContent: null
+      mdContent: null,
+      isOpen: false
     };
   },
   components: {
@@ -39,6 +44,9 @@ export default {
       api.forum
         .getMdFile(this.content)
         .then(response => (this.mdContent = response.data));
+    },
+    displayPost: function() {
+      this.isOpen = true;
     }
   },
   created() {
@@ -49,7 +57,16 @@ export default {
 
 <style>
 .post {
-  max-height: 250px;
+  margin-top: 1rem;
+  padding: 1rem 0.5rem 0.5rem 0.9rem;
+}
+.hidden {
+  max-height: 15rem;
   overflow: hidden;
+  -webkit-box-shadow: 0px 3px 5px 0px rgba(71, 70, 70, 1);
+  -moz-box-shadow: 0px 3px 5px 0px rgba(71, 70, 70, 1);
+  box-shadow: 0px 3px 1px 0px rgba(71, 70, 70, 0.7);
+}
+.displayed {
 }
 </style>
