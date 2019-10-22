16146da84ace618e5038265cb1d3c0a432022b70
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-10-22 18:18:21 -0300

    Add fade effect

--- src/components/for-layout/ForumPostOverview.vue
+++ src/components/for-layout/ForumPostOverview.vue
@@ -1,11 +1,11 @@
 <template>
   <div :class="isOpen ? 'displayed' : 'hidden'" class="post-box">
-    <div class="media">
+    <div class="media" :class="isOpen === false ? 'fade' : ''">
       <div class="media-content post" @click="displayPost">
         <h1 class="subtitle is-size-2">{{ title }}</h1>
         <div class="content">
-          <div v-if="mdContentLoaded">
-            <app-markdown-render :markdown="mdContent"></app-markdown-render>
+          <div v-if="loaded">
+            <app-markdown-render :markdown="content"></app-markdown-render>
           </div>
           <div v-else>
             <b-progress></b-progress>
@@ -21,13 +21,13 @@
 
 <script>
 import AppMarkdownRender from "@/components/for-ui/AppMarkdownRender";
-import api from "@/api";
 export default {
   name: "ForumPostOverview",
   data() {
     return {
       mdContent: null,
-      isOpen: false
+      isOpen: false,
+      loaded: true
     };
   },
   components: {
@@ -39,28 +39,11 @@ export default {
     content: String
   },
   methods: {
-    getMd: function() {
-      api.forum
-        .getMdFile(this.content)
-        .then(response => (this.mdContent = response.data));
-    },
     displayPost: function() {
       this.isOpen = true;
     },
     collapsePost: function() {
       this.isOpen = false;
-    }
-  },
-  created() {
-    this.getMd();
-  },
-  computed: {
-    mdContentLoaded: function() {
-      if (this.mdContent !== null) {
-        return true;
-      } else {
-        return false;
-      }
     }
   }
 };
@@ -80,6 +63,8 @@ export default {
   -moz-box-shadow: 0px 3px 5px 0px rgba(71, 70, 70, 1);
   box-shadow: 0px 3px 1px 0px rgba(71, 70, 70, 0.7);
 }
-.displayed {
+.fade {
+  background: linear-gradient(transparent 150px, white);
+  background: linear-gradient(rgba(0, 0, 0, 0), #000);
 }
 </style>
