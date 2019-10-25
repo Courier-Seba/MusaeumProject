def17b12c2ea8a524bcc4ff08f26f67a5a11fa3d
sebastianvaldivita@gmail.com <sebastianvaldivita@gmail.com>
2019-10-25 10:56:24 -0300

    Start post modal

--- src/views/ForumView.vue
+++ src/views/ForumView.vue
@@ -12,6 +12,48 @@
       :text="$t('forumView.newPost')"
       @pressed="newPost"
     ></app-float-buttom>
+    <b-modal
+      :active.sync="isPostModalActive"
+      has-modal-card
+      full-screen
+      :can-cancel="false"
+    >
+      <div class="modal-card" style="width: auto">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Login</p>
+        </header>
+        <section class="modal-card-body">
+          <b-field label="Email">
+            <b-input
+              type="email"
+              :value="email"
+              placeholder="Your email"
+              required
+            >
+            </b-input>
+          </b-field>
+
+          <b-field label="Password">
+            <b-input
+              type="password"
+              :value="password"
+              password-reveal
+              placeholder="Your password"
+              required
+            >
+            </b-input>
+          </b-field>
+
+          <b-checkbox>Remember me</b-checkbox>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button" type="button" @click="$parent.close()">
+            Close
+          </button>
+          <button class="button is-primary">Login</button>
+        </footer>
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -21,12 +63,20 @@ import AppFloatButtom from "@/components/for-ui/AppFloatButtom";
 import { mapGetters, mapActions } from "vuex";
 export default {
   name: "ForumView",
+  data() {
+    return {
+      isPostModalActive: false
+    };
+  },
   components: {
     ForumPostOverview,
     AppFloatButtom
   },
   methods: {
-    ...mapActions(["getLastestPosts"])
+    ...mapActions(["getLastestPosts"]),
+    newPost: function() {
+      this.isPostModalActive = true;
+    }
   },
   computed: {
     ...mapGetters(["postList"])
