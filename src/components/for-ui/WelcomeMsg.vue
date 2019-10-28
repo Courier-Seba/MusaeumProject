647443a3c77f6eaa561eaf78bf3c06c2f23e7cf8
Seba Valdivia <sebastianvaldivita@gmail.com>
2019-10-28 20:28:27 -0300

    Fix ugly home

--- src/components/for-ui/WelcomeMsg.vue
+++ src/components/for-ui/WelcomeMsg.vue
@@ -1,11 +1,15 @@
 Welcome mensage of the page.
 <template>
-  <div id="welcome" class="container has-text-dark">
-    <h1 class="is-size-2">{{ $t("welcome.title") }}</h1>
-    <p class="is-size-4">{{ $t("welcome.intro") }}</p>
-    <hr>
-    <h6 class="is-size-6 has-text-black">{{ $t("welcome.outro") }}</h6>
-    <h6 class="is-size-7 has-text-grey">- Rodrigo Barceló</h6>
+  <div id="welcome" class="hero is-large">
+    <section class="hero-body">
+      <div class="container">
+        <h1 class="is-size-2">{{ $t("welcome.title") }}</h1>
+        <p class="is-size-4">{{ $t("welcome.intro") }}</p>
+        <hr />
+        <h6 class="is-size-6 has-text-black">{{ $t("welcome.outro") }}</h6>
+        <h6 class="is-size-7 has-text-grey">- Rodrigo Barceló</h6>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -14,9 +18,3 @@ export default {
   name: "WelcomeMsg"
 };
 </script>
-
-<style>
-#welcome {
-  padding: 10rem;
-}
-</style>
