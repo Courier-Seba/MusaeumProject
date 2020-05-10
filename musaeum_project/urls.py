c2d901ed648d4ee981be68fc47144775047837e8
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-05-10 15:56:42 -0300

    Move to simple jwt

--- musaeum_project/urls.py
+++ musaeum_project/urls.py
@@ -23,7 +23,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
 ]
 
 # Develop urls
