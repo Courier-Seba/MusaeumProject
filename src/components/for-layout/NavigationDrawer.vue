396379a1964cbad9a66fbd7b31800d1b12b689aa
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-05-17 18:12:38 -0300

    Move drawer to component / layout

---
+++ src/components/for-layout/NavigationDrawer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-navigation-drawer
+    v-if="true"
+    mini-variant
+    expand-on-hover
+    overflow
+    color="primary darken-1"
+    app
+  ></v-navigation-drawer>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "NavigationDrawer",
+  computed: {
+    ...mapGetters(["userIsLogged"]) // TODO: Conditional reder if is loged only
+  }
+}
+</script>
+
+<style>
+
+</style>
