50ba7adf028ed98f5c4426f007a2c67ae8d05e1d
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-05-17 17:17:11 -0300

    Add home structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <v-container fluid id="home-view">
+    <v-row>
+      <v-col>
+        <v-card>
+          Musaeum
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          Login
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+</style>
