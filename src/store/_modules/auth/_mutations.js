b12912246fb317b5c2c8dd654f72282c94c145b3
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-06-15 14:22:06 -0300

    Remove vue-cookie actions in store

--- src/store/_modules/auth/_mutations.js
+++ src/store/_modules/auth/_mutations.js
@@ -1,17 +1,12 @@
-import vue from "vue";
-
 const mutations = {
   savePK(state, payload) {
     state.id = payload;
-    vue.prototype.$cookie.set("id", payload, 7);
   },
   saveJWT(state, payload) {
     state.jwt = payload;
-    vue.prototype.$cookie.set("token", payload, 7);
   },
   saveRefreshJWT(state, payload) {
     state.refreshJwt = payload;
-    vue.prototype.$cookie.set("refreshToken", payload, 7);
   },
   activateUser(state) {
     state.isLogged = true;
