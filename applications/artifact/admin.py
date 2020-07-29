c6cc949d5dde4ba0295cdf6368f9e456f61f418b
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-07-29 20:32:06 -0300

    Add artifact image and comment to admin site

--- applications/artifact/admin.py
+++ applications/artifact/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import Artifact, ArtifactTag, ArtifactCollection
+from .models import (
+    Artifact,
+    ArtifactTag,
+    ArtifactCollection,
+    ArtifactImage,
+    ArtifactComment
+)
 # Register your models here.
 admin.site.register(Artifact)
 admin.site.register(ArtifactTag)
 admin.site.register(ArtifactCollection)
+admin.site.register(ArtifactComment)
+admin.site.register(ArtifactImage)
 
 
