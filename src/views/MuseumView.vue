4832aa9ddb3f638de98664ef7f463449919b1d21
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-08-03 17:33:32 -0300

    Add loading effect and Refactor magic api call

--- src/views/MuseumView.vue
+++ src/views/MuseumView.vue
@@ -1,81 +1,48 @@
 <template>
-  <v-container>
-    <v-card class="mx-auto" max-width="100%">
-      <v-toolbar color="indigo" dark>
-        <v-toolbar-title v-t="{ path: 'mymuseum.general' }"></v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn icon disabled>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-      </v-toolbar>
-
-      <v-container fluid>
-        <v-row dense>
-          <v-col v-for="artifact in artifactList" :key="artifact.id">
-            <v-card>
-              <v-img
-                :src="artifact.picture"
-                class="white--text align-end"
-                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                height="200px"
-              >
-                <v-card-title v-text="artifact.name"></v-card-title>
-              </v-img>
-
-              <v-card-actions>
-                <v-spacer></v-spacer>
-
-                <v-btn icon>
-                  <v-icon>mdi-heart</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                  <v-icon>mdi-bookmark</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                  <v-icon>mdi-share-variant</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
-  </v-container>
+  <v-container fluid>
+    <v-overlay :value="loading" absolute>
+    <v-progress-circular
+      :size="70"
+      :width="7"
+      color="purple"
+      indeterminate
+    ></v-progress-circular>
+  </v-overlay>
+  <v-row dense>
+    <v-col v-for="artifact in artifactList" :key="artifact.id">
+      <v-card>
+      </v-card>
+    </v-col>
+  </v-row>
+</v-container>
 </template>
 
 <script>
-import axios from "axios";
+import api from "@/api"
 export default {
-  name: "MuseumView",
-  props: {
-    id: {
-      required: true
-    }
-  },
+name: "MuseumView",
+props: {
+  id: {
+    required: true
+  }
+},
+data: () => ({
+  museumData: null,
+  artifactList: null,
+  loading: false
+}),
 
-  data: () => ({
-    museumData: null,
-    artifactList: null
-  }),
-
-  methods: {
-    collectUserMuseum: function() {
-      let url = "http://localhost:8000/api/v1/museum/" + this.id + "/";
-      axios.get(url).then(response => {
-        this.museumData = response.data;
-        axios
-          .get(
-            "http://localhost:8000/api/v1/artifact/?collection=&museum__id=1"
-          )
-          .then(response => (this.artifactList = response.data.results));
+methods: {
+  collectUserMuseum: function() {
+    api.artifact.getListArtifactOfMuseum(this.id)
+      .then(response => {
+        this.artifactList = response.data.results;
+        this.loading = false;
       });
     }
   },
   created() {
+    this.loading = true;
     this.collectUserMuseum();
   }
 };
