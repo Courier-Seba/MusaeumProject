f030d39d53e0b44f5bf0636ea02ffbe476f4010e
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-08-03 18:14:09 -0300

    Add paralax view of museum picture

--- src/views/MuseumView.vue
+++ src/views/MuseumView.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container fluid>
-    <v-overlay :value="loading" absolute>
+<div>
+  <v-overlay :value="loading" absolute>
     <v-progress-circular
       :size="70"
       :width="7"
@@ -8,6 +8,24 @@
       indeterminate
     ></v-progress-circular>
   </v-overlay>
+
+  <v-parallax
+    v-if="museumData.front_picture !== null"
+    :src="museumData.front_picture"
+    height="300"
+  >
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-thin mb-4">{{ museumData.name }}</h1>
+        <h2 class="subheading">{{ museumData.country }} {{ museumData.city }}</h2>
+      </v-col>
+    </v-row>
+  </v-parallax>
+
+  <v-container>
+  <v-row>
+  </v-row>
+
   <v-row dense>
     <v-col v-for="artifact in artifactList" :key="artifact.id">
       <v-card>
@@ -15,35 +33,43 @@
     </v-col>
   </v-row>
 </v-container>
+</div>
 </template>
 
 <script>
 import api from "@/api"
 export default {
-name: "MuseumView",
-props: {
-  id: {
-    required: true
-  }
-},
-data: () => ({
-  museumData: null,
-  artifactList: null,
-  loading: false
-}),
-
-methods: {
-  collectUserMuseum: function() {
-    api.artifact.getListArtifactOfMuseum(this.id)
-      .then(response => {
-        this.artifactList = response.data.results;
-        this.loading = false;
-      });
+  name: "MuseumView",
+  props: {
+    id: {
+      required: true
     }
   },
-  created() {
+  data: () => ({
+    museumData: {
+      name: "",
+      front_picture: null,
+      country: "",
+      city: "",
+    },
+    artifactList: null,
+    loading: false
+  }),
+  methods: {
+    collectMuseumData: function() {
+      api.museum.getMuseumData(this.id).then(response => {
+        this.museumData = response.data
+        api.artifact.getListArtifactOfMuseum(this.id)
+          .then(response => {
+            this.artifactList = response.data.results;
+            this.loading = false;
+        });
+      })
+    }
+  },
+  beforeMount() {
     this.loading = true;
-    this.collectUserMuseum();
+    this.collectMuseumData();
   }
 };
 </script>
