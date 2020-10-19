00ad8fdfdf9075912c34345c6bb6697284dd9f03
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-10-18 23:15:14 -0300

    Move navbar and Add raw lang button

---
+++ client/src/components/for-layout/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+<v-app-bar app color="primary lighten-1">
+  <v-img
+    class="mx-2"
+    src="/favicon.ico"
+    contain
+    max-height="40"
+    max-width="40"
+  ></v-img>
+  <v-toolbar-title>MUSAEUM</v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-btn
+    fab
+    dark
+    small
+    color="info"
+  >
+  </v-btn>
+</v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+
+}
+</script>
+
+<style>
+
+</style>
+
