434182dde08594428e420ef00b9e7367280f5bbb
Jvtomas98 <-e>
2020-10-21 20:20:19 -0300

    change footer

---
+++ client/src/components/for-layout/FooterDiv.vue
@@ -0,0 +1,14 @@
+<template>
+      <v-footer inset app>
+          <v-spacer></v-spacer>
+      <span class="px-4">Musaeum &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: "FooterDiv",
+}
+</script>
+
+<style></style>
