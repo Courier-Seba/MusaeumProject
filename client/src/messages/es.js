8ebbf986de15bfe7e7d139de04f92764d7154737
Jvtomas98 <-e>
2020-10-23 19:49:10 -0300

    Adding information about-us

--- client/src/messages/es.js
+++ client/src/messages/es.js
@@ -56,5 +56,18 @@ export default {
       "Ayudanos a construir está pagina, lo único que tienes que hacer es empezar a compartir. Es gratis, nunca serás obligado a pagar. Ayudanos a construir una ÚNICA cultura al rededor del mundo",
     moreInfo: "Para obtener mas información visite 'About Us' en la parte inferior",
   },
+aboutUsView: {
+  objetiveTitle: "Quienes somos",
+  objetiveDescription: "Con el objetivo de proteger la cultura del hombre tuvimos la incentiva de desarrollar una solución a nuestro alcance para que la internet deje de ser una construcción sólo para el ocio y el trabajo sino también para compartir la historia y defender su acceso a la gente.",
+  objetiveMain: "Ya que nos encontramos a tanta distancia de cada uno, queremos buscar una manera de conectarnos entre todos por medio de la historia. Donde personas de cualquier lugar puedan acceder y mostrar su historia, cultura o religión ya sea de tiempos modernos o de tiempos anteriores",
+  justificationTitle: "Por qué lo hacemos",
+  justificationDescription: "Ante el avance de la tecnología vimos un olvido por la cultura general. Con la virtualización la gente empezó a determinar que si no existía un registro digital, no existía en el mundo real.",
+  justificationHistory: "La historia, algo que nos ayuda a aprender de nuestros errores, tiene una gran importancia para la humanidad y el único lugar donde se almacenan toda la evidencia de lo que paso esta en los museos, exhibiciones y monumentos. Esa evidencia son miles de piezas y textos que nos recuerdan cómo llegamos hasta aquí y por lo que pasamos.",
+  userInvitationTitle: "Que buscamos",
+  userHelp:"Ayudanos hoy contribuyendo con lo que pienses que sea necesario para que los demás aprendan y nunca nos olvidemos de los mas importante. De donde venimos, que hacemos ahora, y que podríamos hacer mañana ",
+  userInvitation:"Contribuir significa algo para nostros, para vos y para la persona que se va a conectar con lo que vos escribis. Unite ahora mismo gratis y comenza a ampliar tus conocimientos culturales",
+
+
+}
 };
 
--- client/src/views/AboutUsView.vue
+++ client/src/views/AboutUsView.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>BOCA</h1>
+  <v-container>
+    <v-row>
+      <v-card-title v-t="{ path: 'aboutUsView.objetiveTitle' }"></v-card-title>
+      <v-card-text v-t=" {path: 'aboutUsView.objetiveDescription'} "></v-card-text>
+      <v-card-text v-t=" {path: 'aboutUsView.objetiveMain'} "></v-card-text>
+    </v-row>
+    <v-row>
+      <v-card-title v-t=" {path: 'aboutUsView.justificationTitle'} "></v-card-title>
+      <v-card-text v-t=" {path: 'aboutUsView.justificationDescription'} "></v-card-text>
+      <v-card-text v-t=" {path: 'aboutUsView.justificationHistory'} "></v-card-text>
+    </v-row>
+    <v-row>
+      <v-card-title v-t=" {path: 'aboutUsView.userInvitationTitle'} "></v-card-title>
+      <v-card-text v-t=" {path: 'aboutUsView.userHelp'} "></v-card-text>
+      <v-card-text v-t=" {path: 'aboutUsView.userInvitation'} "></v-card-text>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
