ae3ba01c51119a7c7539924dcfda572f6180f52f
Jvtomas98 <-e>
2020-10-23 19:14:37 -0300

    Starts 404 page

---
+++ client/src/views/NotFoundView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <center>
+            <h1>404 not found</h1>
+            <h2>it seems you're in the wrong page</h2>
+        </center>
+    </div>
+</template>
+
+<script>
+export default {
+name: NotFoundView,
+}
+</script>
+
+<style>
+
+</style>
