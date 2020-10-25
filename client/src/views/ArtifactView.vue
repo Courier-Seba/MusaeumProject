4aa78c9364d685081ef8520d177d618e5aa0006f
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-10-25 20:41:05 -0300

    Add method to retriver username

--- client/src/views/ArtifactView.vue
+++ client/src/views/ArtifactView.vue
@@ -30,7 +30,7 @@
     >
       <v-col cols="12">
         <artifact-comment
-          :username="comment.user"
+          :username="commentUsername(comment.user)"
           :message="comment.message"
         ></artifact-comment>
       </v-col>
@@ -66,6 +66,12 @@ export default {
       artifactImages: [],
       artifactComments: [],
       addCommentDialog: false,
+    }
+  },
+  computed: {
+    commentUsername(user) {
+      console.alert(user)
+      return ""
     }
   },
   methods: {
