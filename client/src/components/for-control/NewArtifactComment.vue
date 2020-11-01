a47d86c91f14e1ed7342601eb200ada4b4536014
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-11-01 08:23:34 -0300

    Add reponsivenes to artifact comment

--- client/src/components/for-control/NewArtifactComment.vue
+++ client/src/components/for-control/NewArtifactComment.vue
@@ -1,17 +1,15 @@
 <template>
-    <div>
-        <div>
-            <v-card-text>
-              <v-textarea :label="$t('comment.addNew')"> </v-textarea>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="accent" @click="submitComment">
-                <span v-t="{ path: 'general.acceptButton' }"></span>
-              </v-btn>
-          </v-card-actions>
-        </div>
-    </div>
+  <div class="mx-auto">
+    <v-card-text>
+      <v-textarea :label="$t('comment.addNew')"> </v-textarea>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="accent" @click="submitComment">
+        <span v-t="{ path: 'general.acceptButton' }"></span>
+      </v-btn>
+    </v-card-actions>
+  </div>
 </template>
 
 <script>
