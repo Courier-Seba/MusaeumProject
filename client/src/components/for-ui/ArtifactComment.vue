6b363b293bd7a4788ebac9b2f6d5360ef382f994
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-11-01 08:13:11 -0300

    Add display of username to artifact

--- client/src/components/for-ui/ArtifactComment.vue
+++ client/src/components/for-ui/ArtifactComment.vue
@@ -4,7 +4,7 @@
     max-width="500"
     outlined
   >
-    <v-card-title>{{ user }} dice: </v-card-title>
+    <v-card-title>{{ username }} dice: </v-card-title>
     <v-card-text>
       <p>{{ message }}</p>
     </v-card-text>
@@ -12,11 +12,23 @@
 </template>
 
 <script>
+import api from "@/api";
+
 export default {
   name: "ArtifactComment",
   props: {
-    username: String,
-    message: String
+    user: String,
+    message: String,
+  },
+  data() {
+    return {
+      username: "Anon"
+    }
+  },
+  beforeMount() {
+    api.user.getUsername(this.user).then(response => 
+      this.username = response.data.username
+    )
   }
 }
 </script>
