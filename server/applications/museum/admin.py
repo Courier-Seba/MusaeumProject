aa68131362955b8e58e93f62e29633d7aeb52727
Seba Valdivia <sebastianvaldivita@gmail.com>
2020-11-04 15:21:55 -0300

    Add museum visit model to admin site

--- server/applications/museum/admin.py
+++ server/applications/museum/admin.py
@@ -3,10 +3,11 @@ Registration of models to web admin page
 """
 from django.contrib import admin
 
-from .models import Museum, MuseumStar, MuseumType, MuseumLevel
+from .models import Museum, MuseumStar, MuseumType, MuseumLevel, MuseumVisit
 
 admin.site.register(Museum)
 admin.site.register(MuseumStar)
 admin.site.register(MuseumType)
 admin.site.register(MuseumLevel)
+admin.site.register(MuseumVisit)
 
